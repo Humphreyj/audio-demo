2801b10549bdf039ec7e6edce5a20d1bb8e018e4
Joshua Humphrey <joshhumphrey1@gmail.com>
2021-03-09 02:10:30 -0600

    added button to toggle text description

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const ItemCard = (props) => {
-    const {img, name, description, audio, abv} = props;
-    return (
-        <div className='item-card'>
-            <img src={img} alt=""/>
-            <h1 className="item-name">{name}</h1>
+  const { img, name, description, audio} = props;
+  const [showDescription, setShowDescription] = useState(false);
 
-            <audio src={audio} controls></audio>
-            <p className="abv">ABV: {abv}%</p>
-            <p className="description">{description}</p>
+  const toggleDescription = () => {
+      setShowDescription(!showDescription)
+  }
+  return (
+    <div className="item-card">
+      <img src={img} alt="" />
+      <h1 className="item-name">{name}</h1>
 
-            
-
-            
-        </div>
-    );
-}
+      <audio src={audio} controls></audio>
+      <button className="toggle-description" onClick={toggleDescription}>{ showDescription ? "Hide text description" : "Show text description" }</button>
+      { showDescription ? <p className="description">{description}</p> : null }
+      
+    </div>
+  );
+};
 
 export default ItemCard;
--- src/components/QrScanner.jsx
+++ src/components/QrScanner.jsx
@@ -1,33 +1,60 @@
 import React, { useState } from "react";
 import QrReader from "react-qr-reader";
 import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
 
 const QrScanner = () => {
+  
   let history = useHistory();
   const [showScanner, setShowScanner] = useState(false);
 
+  const errorMessage = () => toast.error('An error has occurred', {
+    position: "top-right",
+    autoClose: 2500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+
   const handleScan = (data) => {
     if (data) {
+     
       let split = data.split("/");
-
-      history.push(`/item/${split[4]}`);
+      if (split.length > 4) {
+        history.push(`/item/${split[4]}`);
+      } else {
+        errorMessage()
+      }
     }
   };
 
-  const handleError = (err) => {
-    console.error(err);
-  };
 
   const toggleScanner = () => {
     setShowScanner(!showScanner);
   };
   return (
     <div className="qr-scanner">
+        <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+         />
+      
       <h1 className="section-title">Scan a QR code to get started.</h1>
       {showScanner ? (
         <QrReader
           delay={1000}
-          onError={handleError}
+          onError={errorMessage}
           onScan={handleScan}
           style={{ width: "90%" }}
         />
